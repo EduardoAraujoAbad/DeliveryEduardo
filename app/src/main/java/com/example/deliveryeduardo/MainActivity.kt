c8b229fc53bee536c6f314682afddc7b4406d1c6
EduardoAraujoAbad <edaraujoab@uide.edu.ec>
2022-05-10 13:58:21 -0500

    actualizacion 10/5/22

--- app/src/main/java/com/example/deliveryeduardo/MainActivity.kt
+++ app/src/main/java/com/example/deliveryeduardo/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.deliveryeduardo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.deliveryeduardo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var views: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        views = ActivityMainBinding.inflate(layoutInflater)
+        initializationListeners()
+        setContentView(views.root)
+    }
+    private fun initializationListeners(){
+        views.buttonLogin.setOnClickListener{
+            Toast.makeText(this,"El usuario es:"+" "+ views.editTextTextPersonName.text+" "+"y su contrasena es"+" "+views.editTextPassword.text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
